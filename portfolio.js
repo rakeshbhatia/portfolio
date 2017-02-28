0fc8cc57317e12e382a806fef32c6b3027258770
Rakesh <rakeshbhatia87@gmail.com>
2017-02-28 11:52:45 -0800

    Completely revamped the whole site

--- portfolio.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('#full_page').fullpage({
-	  sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
-		anchors: ['first_section', 'second_section', 'third_section', 'fourth_section', 'fifth_section'],
-		menu: '#sidebar-wrapper',
-		scrollOverflow: true
-	});
-});
